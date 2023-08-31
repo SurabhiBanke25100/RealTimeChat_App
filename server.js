388ca1b7e8985426611111b54e21dc461ab56ac2
su25100 <surbhibanke2599@gmail.com>
2023-09-01 00:54:32 +0530

    connetion done final commit

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
-const app = require("express");
+const express = require("express");
+const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 
 io.on("connection", (socket) => {
-    console.log("Socket is active to be connected");
+    console.log("Connected....");
 
-    socket.on("chat", (payload) => {
-        console.log("payload: ", payload);
-        io.emit("chat", payload);
+    socket.on("message", (msg) => {
+        socket.broadcast.emit('message', msg)
 
     });
+
+});
+
+app.use(express.static(__dirname + '/frontend'));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
 });
 
 
 server.listen(5000, () => {
     console.log(`server active at port:5000`);
+
 });
